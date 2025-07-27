fa0127cbf83eba5d42e4be605e8680cd5e2ac331
Anay Sehgal <anaysehgal1@gmail.com>
2025-07-27 23:39:32 +0530

    Remove unused code

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -3,13 +3,12 @@ import { useRoute } from 'vue-router';
 import { games } from '@/utils/games';
 import Marbles from '@/components/games/Marbles.vue';
 import RLGL from '@/components/games/RLGL.vue';
-import Dalgona from '@/components/games/Dalgona.vue';
 import TugOfWar from '@/components/games/TugOfWar.vue';
 import router from '@/router';
 
 const route = useRoute();
 const game = games.find(g => g.id === Number(route.params.id));
-const comps = { 0: RLGL, 1: Dalgona,  2: Marbles, 4: TugOfWar }
+const comps = { 0: RLGL,  2: Marbles, 4: TugOfWar }
 if(!game) router.push('/games')
 </script>
 <template>
